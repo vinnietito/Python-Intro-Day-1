b6a241386e1a2718311718b55e5ef75b9b318d3b
vinnietito <vincentnjuguna69@gmail.com>
2024-10-30 19:09:00 +0300

    First commit

--- index.py
+++ index.py
@@ -1,6 +1,19 @@
 # python Variables
-number = 2
-name = "My name is Vincent Kimanthi"
-gender = "Male"
+# number = 2
+# name = "My name is Vincent Kimanthi"
+# gender = "Male"
 
-print(number, name, gender)
+# print(number, name, gender)
+
+
+# Python DataTypes 
+# numeric[int, float, complex] - holds numeric values
+# string[str] - holds a sequence of charachters
+# sequence[list, tuple, range] - holds a collection of items
+# mapping[dict] - holds data in a key-value
+# boolean[bool] - holds either true or false
+# set[set] - holds a collection of unique items
+
+languages = ["dart", "Python", "Web", "DB", "SE", "E&PD", 6, True,]
+# print(languages)
+print(languages[5])
